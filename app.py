786dac4242fd9f0aa35b8f978ce651d6d722c797
Hadeeqa Nisa <hadeeqanisa@gmail.com>
2025-10-06 03:59:34 -0700

    Initial commit for Streamlit deployment

--- app.py
+++ app.py
@@ -1,34 +1,53 @@
 import streamlit as st
 import pickle
 import requests
+import gdown
 
+# Google Drive file IDs
+movies_file_id = "1hN780rSbePArfHLiFTh6oZOQOF5dF96R"
+similarity_file_id = "1xjoOee3VFQlArvvAzUOuvtrpb9GzPRwU"
+
+# Download files (will save locally)
+gdown.download(f"https://drive.google.com/uc?id={movies_file_id}", "movies.pkl", quiet=False)
+gdown.download(f"https://drive.google.com/uc?id={similarity_file_id}", "similarity.pkl", quiet=False)
+
+# Load pickle files
+movies = pickle.load(open("movies.pkl", "rb"))
+similarity = pickle.load(open("similarity.pkl", "rb"))
+
+
+
+# -----------------------------
+# TMDB API poster fetch function
+# -----------------------------
 def fetch_poster(movie_id):
-
     response = requests.get(
         f'https://api.themoviedb.org/3/movie/{movie_id}?api_key=ed2067864e3dde5a91dc6753833ee565&language=en-US'
     )
     data = response.json()
     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
-# Define recommendation function
+# -----------------------------
+# Recommendation function
+# -----------------------------
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x:x[1])[1:6]
 
     recommended_movies = []
     recommended_movies_poster = []
+
     for i in movies_list:
-        movie_id = movies.iloc[i[0]].movie_id  # get actual movie_id from dataset
+        movie_id = movies.iloc[i[0]].movie_id  # get actual TMDB movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        # fetch poster from API
         recommended_movies_poster.append(fetch_poster(movie_id))
+
     return recommended_movies, recommended_movies_poster
 
-# Load data
-movies = pickle.load(open('movies.pkl', 'rb'))
-similarity = pickle.load(open('similarity.pkl', 'rb'))
-
+# -----------------------------
+# Streamlit UI
+# -----------------------------
 st.title("Movie Recommender System")
 
 # Dropdown for movie selection
@@ -37,25 +56,12 @@ selected_movie_name = st.selectbox(
     movies['title'].values
 )
 
-# Button to show recommendations
+# Show recommendations when button clicked
 if st.button('Recommend'):
     names, posters = recommend(selected_movie_name)
 
-
-    col1, col2, col3,col4,col5 = st.columns(5)
-    with col1:
-        st.text(names[0])
-        st.image(posters[0])
-    with col2:
-        st.text(names[1])
-        st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
-    with col4:
-        st.text(names[3])
-        st.image(posters[3])
-    with col5:
-        st.text(names[4])
-        st.image(posters[4])
-
+    # Display recommended movies and posters
+    cols = st.columns(5)
+    for idx, col in enumerate(cols):
+        col.text(names[idx])
+        col.image(posters[idx])
